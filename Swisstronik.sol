71dcc049f7da4a436f585d556937c78698686272
sychonix <110755590+sychonix@users.noreply.github.com>
2023-07-05 20:59:49 +0700

    Update Swisstronik.sol

--- Swisstronik.sol
+++ Swisstronik.sol
@@ -1,52 +1,57 @@
-const social = [
-  { label: 'Twitter', icon: 'tabler:brand-twitter', href: 'https://twitter.com/empowerchain_io' },
-  { label: 'Discord', icon: 'tabler:brand-discord', href: 'https://discord.gg/UTxEzFzHVX' },
-  { label: 'Chrome',  icon: 'tabler:brand-chrome', href: 'https://empowerchain.io/' },
-];
 
-<Layout {meta}>
-  <section>
-    <div class="max-w-6xl mx-auto px-4 sm:px-6">
-      <div class="py-10 md:py-20">
-        <div class="text-center pb-5 md:pb-5 max-w-5xl mx-auto">
-          <Icon name={icon} class="w-20 h-20 inline-block align-text-bottom" />           
-          <h1 class="text-5xl md:text-[3.50rem] font-bold leading-tighter tracking-tighter mb-4 font-heading">
-            Service {name}
-          </h1>
-          <a class="btn bg-primary-400 w-1/4 items-center hover:bg-primary-400 font-bold md:pb-4" href={stake}>
-            Stake with me
-          </a>
-        </div>
-        <section class="bg-primary-100 dark:bg-slate-800 items-center">
-          <div class=" bg-transparent px-4 sm:px-6 py-4 text-md text-center font-medium">
-            <li>
-              <span class="font-bold">Network Type:</span> {network}
-            </li>
-            <li>
-              <span class="font-bold">Chain-id:</span> {chain}
-            </li>
-            <li>
-              <span class="font-bold">Node version:</span> {version}
-            </li>
-            {social.map((item) => (
-              <a href={item.href} target="_blank" rel="noopener noreferrer" key={item.label}>
-                <Icon name={item.icon} class="w-8 h-7 inline-block align-text-bottom" />
-              </a>
-            ))}
-          </div>
-        </section>
-        <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start my-10 dark:text-white">
-          {items.map(({ title, Link }) => (
-            <a href={Link} target="_blank">
-              <div class="relative flex flex-col p-6 bg-white dark:bg-slate-900 rounded shadow-xl hover:shadow-lg transition dark:border dark:border-slate-800">
-                <div class="flex items-center mb-4 text-center">
-                  <div class="ml-4 text-xl font-bold text-center">{title}</div>
-                </div>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  </section>
-</Layout>
+layout: '~/layouts/MarkdownLayout.astro'
+title: Installation Node
+network: Testnet
+icon: elixir
+---
+
+### Install docker compose
+```
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+```
+```
+sudo chmod +x /usr/local/bin/docker-compose
+```
+### Setup your elixir node
+Download the Dockerfile for the Elixir
+```
+wget https://files.elixir.finance/Dockerfile
+```
+### Setup the Dockerfile with your wallet info:
+```
+nano Dockerfile
+```
+and update it from:
+```
+FROM elixirprotocol/validator:testnet-1
+
+ENV ADDRESS=<your_address>
+ENV PRIVATE_KEY=<your_private_key>
+ENV VALIDATOR_NAME=<your_name>
+```
+Then press `ctrl + x`, then `y`, then `enter`
+### Build the Docker image
+```
+docker build . -f Dockerfile -t elixir-validator
+```
+It will take some time to build but once its done you can run the validator by executing the following:
+```
+docker run -it --name ev elixir-validator
+```
+### Enrol your Wallet as a validator
+- Navigate to the [Elixir Network Dashboard](https://dashboard.elixir.finance/) with your wallet-connected browser. Connect with Metamask and switch to the Goerli network.
+
+- Claim your ELXR (If you were part of testnet V1 you would be eligible to receive ELXR). If you need Goerli ETH, you can ask in the [Discord server](https://discord.gg/puzc28Xq).
+
+- Once you have received ELXR tokens, you will need to stake those tokens. Any amount from 100–1000 ELXR will do.
+
+- Next, enroll your validator. You can click the enroll button, and then enter the wallet address used to spin up your validator in the prior steps. Note: there can be a delay before the enrol button becomes active.
+
+- Once this transaction is confirmed, check the [leaderboard](https://dashboard.elixir.finance/leaderboard) to ensure your address is enrolled.
+
+- Then, verify that valid orders are being processed by finding your validator on the [metrics page](https://dashboard.elixir.finance/metrics).
+
+# check log node
+```
+docker logs -f <Container ID>
+```
